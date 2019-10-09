ed4685858b28f27648de217c329a248f712c4770
Akshay Raichur <akshay.raichur99@gmail.com>
2019-10-09 21:03:16 +0530

    added coments

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,8 @@
+# takes an input string from user
 name = input('Enter any string to check for palindrome : ')
 
-if name == name[::-1]:
-    print('{} is a palindrome'.format(name))
+if name == name[::-1]:  # compares the string with its reverse
+    print('{} is a palindrome'.format(name))  # prints the result
 else:
     print('{} is not a palindrome'.format(name))
 
